1cfa71b04a1dc80ab354023e485751e97da8bb51
M. Messmari <mussmesmari.developer@gmail.com>
2020-04-13 16:51:19 +0200

    ShoppingCart class added to eShop.Data | Not complete

---
+++ eShop.Data/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,39 @@
+﻿using eShop.Data.Data;
+using eShop.infrastructure.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.ShoppingCart
+{
+    public class ShoppingCart
+    {
+        private readonly eShopDbContext _eShopDbContext;
+
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        private ShoppingCart(eShopDbContext eShopDbContext)
+        {
+            _eShopDbContext = eShopDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+
+            var context = services.GetService<eShopDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+    }
+} 
