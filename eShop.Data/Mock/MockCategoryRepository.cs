89b5c6122f3dc3315529c8237bb4a759f9df1de8
M. Messmari <mussmesmari.developer@gmail.com>
2020-04-15 09:35:29 +0200

    Cleaning up the master

--- eShop.Data/Mock/MockCategoryRepository.cs
+++ eShop.Data/Mock/MockCategoryRepository.cs
@@ -10,15 +10,12 @@ namespace eShop.Data.Mock
     public class MockCategoryRepository : ICategoryRepository
     {
         public IEnumerable<Category> AllCategories => new List<Category>
-    {
+        {
                 new Category{CategoryId=1, CategoryName="Category One", Description="All events of category one"},
                 new Category{CategoryId=2, CategoryName="Category Two", Description="All events of category two"},
                 new Category{CategoryId=3, CategoryName="Category Three", Description="All events of category three"}
-    };
+        };
 
-        public List<SelectListItem> PopulateCategories()
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
