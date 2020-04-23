d338050f445f671fb7e7d162ea22c5adc319686a
M. Messmari <mussmesmari.developer@gmail.com>
2020-04-23 18:30:08 +0200

    Total currency format fixed in the shopping cart

--- ViewModels/ShoppingCartViewModel.cs
+++ ViewModels/ShoppingCartViewModel.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using eShop.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace eShop.Web.ViewModels
 {
     public class ShoppingCartViewModel
     {
         public ShoppingCart ShoppingCart { get; set; }
-        public decimal ShoppingCartTotalSEK { get; set; }
+        public decimal ShoppingCartTotalSEK { get; set; }        
         public decimal ShoppingCartTotalEUR { get; set; }
     }
 }
