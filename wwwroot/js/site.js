4eb95e299f39beb40aac12d82bf1291480f09313
M. Messmari <mussmesmari.developer@gmail.com>
2020-05-06 16:10:08 +0200

    Event cart and details page as well as the footer fixed

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+$(document).ready(function () {
+    console.log("document is ready");
+
+    $(".card").hover(
+        function () {
+            $(this).addClass('shadow-lg').css('cursor', 'pointer');
+        }, function () {
+            $(this).removeClass('shadow-lg');
+        }
+    );
+});
