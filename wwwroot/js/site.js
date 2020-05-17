2b9fb5e7ab438ee6d9ada3ad31b9a2e941e3fd9b
M. Messmari <mussmesmari.developer@gmail.com>
2020-05-17 21:04:47 +0200

    Font end: Card hover Filter Search bar Details page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,6 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 
+// Hover Card effect //
 $(document).ready(function () {
     console.log("document is ready");
 
@@ -13,3 +14,37 @@ $(document).ready(function () {
         }
     );
 });
+/* -------------- */
+
+
+
+/* goToTopButton */
+var goToTopButton = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopButton.style.display = "block";
+    } else {
+        goToTopButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+/* -------------- */
+
+
+
+
+// scroll spy the event detail page
+$('body').scrollspy({ target: '#side-menu-list'})
+/* -------------- */
+
+
+
